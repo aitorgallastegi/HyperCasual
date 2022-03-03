ac6a6db94cfb20824c5f18b837be706357463a49
aitor <aitor51@outlook.com>
2022-03-03 23:42:04 +0100

    upload
    primera subida

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour{
+
+    public GameObject groundTile;
+    Vector3 nextSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start(){
+        SpawnTile();
+        SpawnTile();
+        SpawnTile();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    void SpawnTile(){
+        //GameObject temp = (groundTile, nextSpawnPoint, Quaternion.Identity);
+        //nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+    }
+}
